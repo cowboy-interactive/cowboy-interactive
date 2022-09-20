2a9912cd0dce4334b38a977f6d1e206582244fef
Julian Mayes <juliantmayes@gmail.com>
2022-09-20 12:17:04 -0700

    document

--- pages/index.js
+++ pages/index.js
@@ -124,15 +124,15 @@ export default function Home() {
           small={"margin:0 0 60px 0; align-items: center; text-align: center;"}
           xsmall={"align-items: flex-start; text-align: left;"}
         >
-          <H3 all={"margin: 0 0 20px 0; maxWidth: 800px;"}>Why Us</H3>
-          <H2 all={"margin: 0 0 40px 0; maxWidth: 800px;"}>
+          <H3 all={"margin: 0 0 20px 0; max-width: 800px;"}>Why Us</H3>
+          <H2 all={"margin: 0 0 40px 0; max-width: 800px;"}>
             An exceptional quality <br /> that can't be beaten
           </H2>
-{/*           <Text all={"margin: 0 0 60px 0; max-width: 530px;"}>
+          <Text all={"margin: 0 0 60px 0; max-width: 530px;"}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
             feugiat pharetra quis imperdiet cursus tristique tincidunt non
             vitae.
-          </Text> */}
+          </Text>
           <Container all={"flex-direction: row"}>
             <Button all={`background: ${themes.button}; color: #fff;`}>
               Get a Quote
