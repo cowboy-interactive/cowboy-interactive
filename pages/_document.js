705f83bed9e3c709f641b9ed4ade8aad8dde11e7
Julian Mayes <juliantmayes@gmail.com>
2022-11-07 13:32:04 -0800

    meta description

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,7 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#fff" />
           <meta
             name="description"
-            content="Cowboy Interactive · Web Design and Development for your small business. We build, host and manage fully customized websites starting at $250 CAD/mo."
+            content="Cowboy Interactive · Affordable web design and development for your small business. We build host and manage custom websites starting at $250 CAD/mo."
           ></meta>
         </Head>
         <body>
